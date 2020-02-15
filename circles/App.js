96e7d589b2426aa58e97f083ffb23c1329253ded
kev <45981465+abbo-1@users.noreply.github.com>
2020-02-15 12:58:27 -0600

    circles

--- circles/App.js
+++ circles/App.js
@@ -1,9 +1,42 @@
 // Render the circles using React!
 const App = (props) => {
     let { circles } = props;
+
+    console.log(props)
+
+    // const myStyle = {
+    //     width: circles[0].radius * 2, 
+    //     height: circles[0].radius * 2,
+    //     borderRadius: circles[0].radius,
+    //     backgroundColor: circles[0].color
+    // }
+
+    const myStyle = {
+        width: circles.radius, 
+        height: circles.radius,
+        borderRadius: circles.radius,
+        backgroundColor: circles.color
+    }
+
+    superCircles = <div style={myStyle} />
+
+    const circleGathering = []
+        for (var i=0; i<12; i++) {
+            circleGathering.push(<superCircles key={i} />)
+        }
+
     return (
-        <span>circles</span>
+        <div {...circleGathering} />
     )
 }
 
-const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
+// const Circle = null;
+
+// CHALLENGE: Write a separate Circle component for use in the App component
+
+// {{
+//     width: 50, 
+//     height: 50,
+//     backgroundColor: "#ffff00",
+//     borderRadius: "50px",
+//     }}>
